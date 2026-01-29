eed39d600d7750a780b3f64c023b16504da7e41d
Jeremy Angele <131715596+angelej@users.noreply.github.com>
2026-01-29 14:36:34 +0100

    wip: improve credential field guessing

--- src/Listeners/AuthenticationSubscriber.php
+++ src/Listeners/AuthenticationSubscriber.php
@@ -2,38 +2,39 @@
 
 namespace Vulnerar\Agent\Listeners;
 
+use Illuminate\Auth\Events\Attempting;
 use Illuminate\Auth\Events\Failed;
 use Illuminate\Auth\Events\Login;
-use Illuminate\Auth\Events\Registered;
-use Illuminate\Auth\Events\Verified;
 use Illuminate\Events\Dispatcher;
+use Illuminate\Support\Facades\Context;
+use SensitiveParameter;
 use Vulnerar\Agent\Event;
 use Vulnerar\Agent\Jobs\IngestEvents;
 use Vulnerar\Agent\Vulnerar;
 
 class AuthenticationSubscriber
 {
-    public function handleRegistered(Registered $event): void
+    public function handleAttempting(Attempting $event): void
     {
-        $event = new Event(
-            'auth.registered',
-            [
-                'login' => request()?->get('email', request()?->get('username', request()?->get('login'))),
-                'user' => Vulnerar::resolveUserDetails($event->user),
-                'ip_address' => request()?->ip(),
-            ],
-
-        );
-        IngestEvents::dispatch($event);
+        // Store the credentials temporarily to be used in subsequent events
+        Context::addHidden('vulnerar_auth_credentials', $event->credentials);
     }
 
     public function handleLogin(Login $event): void
     {
+        $credentials = $this->guessCredentialFields(
+            Context::pullHidden('vulnerar_auth_credentials', [])
+        );
         $event = new Event(
             'auth.login',
             [
                 'guard' => $event->guard,
-                'login' => request()?->get('email', request()?->get('username', request()?->get('login'))),
+                'login' => $credentials['login'] ?? null,
+                'password' => [
+                    'sha1' => isset($credentials['password'])
+                        ? sha1((string) $credentials['password'])
+                        : null,
+                ],
                 'user' => Vulnerar::resolveUserDetails($event->user),
                 'ip_address' => request()?->ip(),
             ]
@@ -47,7 +48,7 @@ class AuthenticationSubscriber
             'auth.failed',
             [
                 'guard' => $event->guard,
-                'credentials' => $event->credentials,
+                'credentials' => $this->guessCredentialFields($event->credentials),
                 'user' => Vulnerar::resolveUserDetails($event->user),
                 'ip_address' => request()?->ip(),
             ]
@@ -55,22 +56,25 @@ class AuthenticationSubscriber
         IngestEvents::dispatch($event);
     }
 
-    public function handleVerified(Verified $event): void
+    /**
+     * Guess the login and password fields from the given credentials.
+     */
+    private function guessCredentialFields(#[SensitiveParameter] array $credentials): array
     {
-        $event = new Event(
-            'auth.verified',
-            [
-                'user' => Vulnerar::resolveUserDetails($event->user), // @phpstan-ignore-line
-                'ip_address' => request()?->ip(),
-            ]
-        );
-        IngestEvents::dispatch($event);
+        $credentials = collect($credentials);
+
+        return [
+            'login' => $credentials->get('email')
+                ?? $credentials->get('username')
+                ?? $credentials->get('login'),
+            'password' => $credentials->get('password'),
+        ];
     }
 
     public function subscribe(Dispatcher $events): array
     {
         return [
-            Registered::class => 'handleRegistered',
+            Attempting::class => 'handleAttempting',
             Login::class => 'handleLogin',
             Failed::class => 'handleFailed',
         ];
